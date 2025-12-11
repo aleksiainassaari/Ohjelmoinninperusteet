f4f937c5e58f9e6fb719fbcddeee9585e4e6b8ab
aleksiainassaari <aleksi.ainassaari@centria.fi>
2025-12-11 11:01:49 +0200

    viikko5b done

--- viikko5/viikko5B/raportti_tiedostoon.py
+++ viikko5/viikko5B/raportti_tiedostoon.py
@@ -1,5 +1,5 @@
-
-
+# Copyright (c) 2025 Aleksi Ainassaari
+# License: MIT
 
 
 
@@ -7,7 +7,6 @@
 
 
 from datetime import datetime, date
-
 
 def muunna_tiedot(tietue: list) -> list:
     """
@@ -28,3 +27,150 @@ def muunna_tiedot(tietue: list) -> list:
         int(tietue[5]),
         int(tietue[6]),
     ]
+
+def lue_data(tiedoston_nimi: str) -> list:
+    """
+    Lukee CSV-tiedoston ja palauttaa rivit sopivassa rakenteessa ja tietotyypeissä.
+    Kutsuu funktiota muunna_tiedot (lst):
+     funktio palauttaa listan -> Tietotyypit muutettu
+
+     Parametrit:
+     tiedoston_nimi (str): ottaa vastaan tiedoston, jossa kentät jaettu merkillä ;
+
+     Palautus:
+     tietokanta (lst): palauttaa tietokannan, jossa tietotyypit on muutettu
+     """
+    tietokanta = []
+    with open(tiedoston_nimi, "r", encoding="utf-8") as f:
+        next(f)  # ohitetaan otsikkorivi
+        for tietue in f:
+            tietue = tietue.split(";")
+            tietokanta.append(muunna_tiedot(tietue))
+    return tietokanta
+
+def paivantiedot(paiva: date, tietokanta: list) -> list:
+    """Laskee kulutus- ja tuotantotiedot vaiheittain ja palauttaa listan
+    Laskettavat tiedot muutetaan Wh -> kWh
+     Parametrit:
+     paiva (date): Raportoitava päivä
+     tietokanta (list): Kulutus- ja tuotantotiedot + päivämäärät
+       Palautus:
+     Listan, jossa tulostettavat merkkijonot """
+
+    kulutus = [0,0,0]
+    tuotanto = [0,0,0]
+
+    for tietue in tietokanta:
+        if tietue[0].date() == paiva:
+            kulutus[0] += tietue[1] / 1000
+            kulutus[1] += tietue[2] / 1000
+            kulutus[2] += tietue[3] / 1000
+            tuotanto[0] += tietue[4] / 1000
+            tuotanto[1] += tietue[5] / 1000
+            tuotanto[2] += tietue[6] / 1000
+
+    return [
+        f"{paiva.day}.{paiva.month}.{paiva.year}",
+        f"{kulutus[0]:.2f}".replace(".", ","),
+        f"{kulutus[1]:.2f}".replace(".", ","),
+        f"{kulutus[2]:.2f}".replace(".", ","),
+        f"{tuotanto[0]:.2f}".replace(".", ","),
+        f"{tuotanto[1]:.2f}".replace(".", ","),
+        f"{tuotanto[2]:.2f}".replace(".", ","),
+    ]
+
+def main():
+    """Ohjelman pääfunktio: lukee datan, laskee yhteenvedot ja tulostaa raportin."""
+   
+    kulutusTuotantoViikko41 = lue_data("viikko41.csv")
+    kulutusTuotantoViikko42 = lue_data("viikko42.csv")
+    kulutusTuotantoViikko43 = lue_data("viikko43.csv")
+    
+    viikko41 = "\nViikon 41 sähkönkulutus ja -tuotanto (kWh, vaiheittain)\n\n"
+    viikko41 += "Päivä\t\tPvm\t\t\tKulutus [kWh]\t\t\tTuotanto [kWh]\n"
+    viikko41 += "\t\t(pv.kk.vvvv)\tv1\t\tv2\t\tv3\t\tv1\t\tv2\t\tv3\n"
+    viikko41 += "----------------------------------------------------------------------------\n"
+    viikko41 += "maanantai\t" + "\t".join(paivantiedot(date(2025, 10, 6), kulutusTuotantoViikko41)) +"\n"
+    viikko41 += "tiistai\t\t" + "\t".join(paivantiedot(date(2025, 10, 7), kulutusTuotantoViikko41)) +"\n"
+    viikko41 += "keskiviikko\t" + "\t".join(paivantiedot(date(2025, 10, 8), kulutusTuotantoViikko41)) +"\n"
+    viikko41 += "torstai\t\t" + "\t".join(paivantiedot(date(2025, 10, 9), kulutusTuotantoViikko41)) +"\n"
+    viikko41 += "perjantai\t" + "\t".join(paivantiedot(date(2025, 10, 10), kulutusTuotantoViikko41)) +"\n"
+    viikko41 += f"{'lauantai':<10}\t" + "\t".join(paivantiedot(date(2025, 10, 11), kulutusTuotantoViikko41)) +"\n"
+    viikko41 += "sunnuntai\t" + "\t".join(paivantiedot(date(2025, 10, 12), kulutusTuotantoViikko41)) +"\n"
+    viikko41 += "----------------------------------------------------------------------------\n"
+    
+    viikko42 = "\nViikon 42 sähkönkulutus ja -tuotanto (kWh, vaiheittain)\n\n"
+    viikko42 += "Päivä\t\tPvm\t\t\tKulutus [kWh]\t\t\tTuotanto [kWh]\n"
+    viikko42 += "\t\t(pv.kk.vvvv)\tv1\t\tv2\t\tv3\t\tv1\t\tv2\t\tv3\n"
+    viikko42 += "----------------------------------------------------------------------------\n"
+    viikko42 += "maanantai\t" + "\t".join(paivantiedot(date(2025, 10, 13), kulutusTuotantoViikko42)) +"\n"
+    viikko42 += "tiistai\t\t" + "\t".join(paivantiedot(date(2025, 10, 14), kulutusTuotantoViikko42)) +"\n"
+    viikko42 += "keskiviikko\t" + "\t".join(paivantiedot(date(2025, 10, 15), kulutusTuotantoViikko42)) +"\n"
+    viikko42 += "torstai\t\t" + "\t".join(paivantiedot(date(2025, 10, 16), kulutusTuotantoViikko42)) +"\n"
+    viikko42 += "perjantai\t" + "\t".join(paivantiedot(date(2025, 10, 17), kulutusTuotantoViikko42)) +"\n"
+    viikko42 += f"{'lauantai':<10}\t" + "\t".join(paivantiedot(date(2025, 10, 18), kulutusTuotantoViikko42)) +"\n"
+    viikko42 += "sunnuntai\t" + "\t".join(paivantiedot(date(2025, 10, 19), kulutusTuotantoViikko42)) +"\n"
+    viikko42 += "----------------------------------------------------------------------------\n"
+    
+    viikko43 = "\nViikon 43 sähkönkulutus ja -tuotanto (kWh, vaiheittain)\n\n"
+    viikko43 += "Päivä\t\tPvm\t\t\tKulutus [kWh]\t\t\tTuotanto [kWh]\n"
+    viikko43 += "\t\t(pv.kk.vvvv)\tv1\t\tv2\t\tv3\t\tv1\t\tv2\t\tv3\n"
+    viikko43 += "----------------------------------------------------------------------------\n"
+    viikko43 += "maanantai\t" + "\t".join(paivantiedot(date(2025, 10, 20), kulutusTuotantoViikko43)) +"\n"
+    viikko43 += "tiistai\t\t" + "\t".join(paivantiedot(date(2025, 10, 21), kulutusTuotantoViikko43)) +"\n"
+    viikko43 += "keskiviikko\t" + "\t".join(paivantiedot(date(2025, 10, 22), kulutusTuotantoViikko43)) +"\n"
+    viikko43 += "torstai\t\t" + "\t".join(paivantiedot(date(2025, 10, 23), kulutusTuotantoViikko43)) +"\n"
+    viikko43 += "perjantai\t" + "\t".join(paivantiedot(date(2025, 10, 24), kulutusTuotantoViikko43)) +"\n"
+    viikko43 += f"{'lauantai':<10}\t" + "\t".join(paivantiedot(date(2025, 10, 25), kulutusTuotantoViikko43)) +"\n"
+    viikko43 += "sunnuntai\t" + "\t".join(paivantiedot(date(2025, 10, 26), kulutusTuotantoViikko43)) +"\n"
+    viikko43 += "----------------------------------------------------------------------------\n"
+       
+    with open("yhteenveto.txt", "w", encoding="utf-8") as f:
+        f.write(viikko41)
+        f.write(viikko42)
+        f.write(viikko43)
+    
+    print("Raportti luotu")
+
+
+    kulutusTuotantoDB = lue_data("viikko41.csv")
+    print("Viikon 41 sähkönkulutus ja -tuotanto (kWh, vaiheittain)", end="\n\n")
+    print("Päivä\t\tPvm\t\tKulutus [kWh]\t\tTuotanto [kWh]")
+    print("\t\t(pv.kk.vvvv)\tv1\tv2\tv3\tv1\tv2\tv3")
+    print("---------------------------------------------------------------------------")
+    print("maanantai\t" + "\t".join(paivantiedot(date(2025, 10, 6), kulutusTuotantoDB)))
+    print("tiistai\t\t" + "\t".join(paivantiedot(date(2025, 10, 7), kulutusTuotantoDB)))
+    print("keskiviikko\t" + "\t".join(paivantiedot(date(2025, 10, 8), kulutusTuotantoDB)))
+    print("torstai\t\t" + "\t".join(paivantiedot(date(2025, 10, 9), kulutusTuotantoDB)))
+    print("perjantai\t" + "\t".join(paivantiedot(date(2025, 10, 10), kulutusTuotantoDB)))
+    print(f"{'lauantai':<10}\t" + "\t".join(paivantiedot(date(2025, 10, 11), kulutusTuotantoDB)))
+    print("sunnuntai\t" + "\t".join(paivantiedot(date(2025, 10, 12), kulutusTuotantoDB)))
+
+
+    kulutusTuotantoDB = lue_data("viikko42.csv")
+    print("\nViikon 42 sähkönkulutus ja -tuotanto (kWh, vaiheittain)", end="\n\n")
+    print("Päivä\t\tPvm\t\tKulutus [kWh]\t\tTuotanto [kWh]")
+    print("\t\t(pv.kk.vvvv)\tv1\tv2\tv3\tv1\tv2\tv3")
+    print("---------------------------------------------------------------------------")
+    print("maanantai\t" + "\t".join(paivantiedot(date(2025, 10, 13), kulutusTuotantoDB)))
+    print("tiistai\t\t" + "\t".join(paivantiedot(date(2025, 10, 14), kulutusTuotantoDB)))
+    print("keskiviikko\t" + "\t".join(paivantiedot(date(2025, 10, 15), kulutusTuotantoDB)))
+    print("torstai\t\t" + "\t".join(paivantiedot(date(2025, 10, 16), kulutusTuotantoDB)))
+    print("perjantai\t" + "\t".join(paivantiedot(date(2025, 10, 17), kulutusTuotantoDB)))
+    print(f"{'lauantai':<10}\t" + "\t".join(paivantiedot(date(2025, 10, 18), kulutusTuotantoDB)))
+    print("sunnuntai\t" + "\t".join(paivantiedot(date(2025, 10, 19), kulutusTuotantoDB)))
+
+    kulutusTuotantoDB = lue_data("viikko43.csv")
+    print("\nViikon 43 sähkönkulutus ja -tuotanto (kWh, vaiheittain)", end="\n\n")
+    print("Päivä\t\tPvm\t\tKulutus [kWh]\t\tTuotanto [kWh]")
+    print("\t\t(pv.kk.vvvv)\tv1\tv2\tv3\tv1\tv2\tv3")
+    print("---------------------------------------------------------------------------")
+    print("maanantai\t" + "\t".join(paivantiedot(date(2025, 10, 20), kulutusTuotantoDB)))
+    print("tiistai\t\t" + "\t".join(paivantiedot(date(2025, 10, 21), kulutusTuotantoDB)))
+    print("keskiviikko\t" + "\t".join(paivantiedot(date(2025, 10, 22), kulutusTuotantoDB)))
+    print("torstai\t\t" + "\t".join(paivantiedot(date(2025, 10, 23), kulutusTuotantoDB)))
+    print("perjantai\t" + "\t".join(paivantiedot(date(2025, 10, 24), kulutusTuotantoDB)))
+    print(f"{'lauantai':<10}\t" + "\t".join(paivantiedot(date(2025, 10, 25), kulutusTuotantoDB)))
+    print("sunnuntai\t" + "\t".join(paivantiedot(date(2025, 10, 26), kulutusTuotantoDB)))
+if __name__ == "__main__":
+    main()
